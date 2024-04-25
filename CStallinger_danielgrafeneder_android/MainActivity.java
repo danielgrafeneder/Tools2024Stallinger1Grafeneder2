f43ed789f04b1d71566df23b1c95925a17b3ba30
Daniel <danielgrafeneder>
2024-04-25 19:36:39 +0200

    added it is bien

--- CStallinger_danielgrafeneder_android/MainActivity.java
+++ CStallinger_danielgrafeneder_android/MainActivity.java
@@ -3,9 +3,11 @@ public void thisIsANewMethod() {
   System.out.println("This line was also added by Member 1");
   System.out.print("Moin");
   System.out.print("Como estas?");
- }
+  System.out.print("Bien");
+}
 public void existingMethod(){
 
   System.out.println("This line was added by Member 1");
+  System.out.println("It is what it is");
 }
 >>>>>>> d034046e22932d798252bbda9ae2259f0b0f5ab5
